386e01bd38f557a49ccdef7d94c0a9507283d861
Bram Smulders <source@bram.is>
2022-11-21 09:04:26 +0100

    code & numbered list

--- packages/cms/schemas/blockContent.js
+++ packages/cms/schemas/blockContent.js
@@ -27,8 +27,12 @@ export default {
         { title: "H3", value: "h3" },
         { title: "H4", value: "h4" },
         { title: "Quote", value: "blockquote" },
+        { title: "Code", value: "code" },
       ],
-      lists: [{ title: "Bullet", value: "bullet" }],
+      lists: [
+        { title: "Bullet", value: "bullet" },
+        { title: "Numbered", value: "number" },
+      ],
       // Marks let you mark up inline text in the block editor.
       marks: {
         // Decorators usually describe a single property – e.g. a typographic
